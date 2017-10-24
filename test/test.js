e819caa1ce60e683636729e7e68ab4522986f2bd
那达 <youxiang.lyx@alipay.com>
2017-10-24 17:12:11 +0800

    added more test cases

--- test/test.js
+++ test/test.js
@@ -2,9 +2,9 @@ const assert = require('assert');
 const moduleDecompose = require('../lib/module-decompose');
 
 describe('ES6', function() {
-  describe('import React from "react"', function() {
-    it('should return import React from "react"', function() {
-      const source = 'import React from "react"';
+  describe('import React from \'react\'', function() {
+    it('should return <import React from \'react\'> for whatever options', function() {
+      const source = 'import React from \'react\'';
       const result = moduleDecompose(source, {
         modules: {
           react: {
@@ -16,6 +16,56 @@ describe('ES6', function() {
       });
 
       assert.equal(source, result);
+    });
+  });
+
+  describe('import { Button } from \'antd\'', function() {
+    it('should return <import Button from \'antd/lib/button\';> when style: false and camel2Dash: true', function() {
+      const source = 'import { Button } from \'antd\'';
+      const target = '\nimport Button from \'antd/lib/button\';';
+      const result = moduleDecompose(source, {
+        modules: {
+          antd: {
+            style: false,
+            components: 'lib',
+            camel2Dash: true,
+          }
+        }
+      });
+
+      assert.equal(result, target);
+    });
+
+    it('should return <import Button from \'antd/lib/button\';import \'antd/lib/button/style/css\';> when style: \'css\' and camel2Dash: true', function() {
+      const source = 'import { Button } from \'antd\'';
+      const target = '\nimport Button from \'antd/lib/button\';\nimport \'antd/lib/button/style/css\';';
+      const result = moduleDecompose(source, {
+        modules: {
+          antd: {
+            style: 'css',
+            components: 'lib',
+            camel2Dash: true,
+          }
+        }
+      });
+
+      assert.equal(result, target);
+    });
+
+    it('should return <import Button from \'antd/lib/button\';import \'antd/lib/button/style\';> when style: true and camel2Dash: true', function() {
+      const source = 'import { Button } from \'antd\'';
+      const target = '\nimport Button from \'antd/lib/button\';\nimport \'antd/lib/button/style\';';
+      const result = moduleDecompose(source, {
+        modules: {
+          antd: {
+            style: true,
+            components: 'lib',
+            camel2Dash: true,
+          }
+        }
+      });
+
+      assert.equal(result, target);
     });
   });
 });
