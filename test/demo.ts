6e29f3b314c432d2eb6c2b7d749d2bb7f35e4723
那达 <youxiang.lyx@alipay.com>
2017-10-25 15:05:53 +0800

    removed useless code

--- test/demo.ts
+++
@@ -1,9 +0,0 @@
-
-import { List as ListItem, InputItem, Picker} from 'antd-mobile';
-import { Hello } from 'antd';
-import _, { Lodash} from 'lodash';
-import * as __ from 'lodash2';
-import * as ___, { Abcd } from 'lodash3';
-import ____ from 'lodash4';
-import xxxf = require('required');
-import './only';
